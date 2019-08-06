b72c7d10a4841f07de212d80850a908b85911417
Anthony Bandolon <abandolon@gmail.com>
2019-08-06 23:47:52 +0200

    updated initial file to run on local host

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :urls
   devise_for :users
-  devise_for :models
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
